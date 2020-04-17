4f59e67a8b4fef2a72a1652ec722f5cb7338cadf
ayush <ayushsahu99@gmail.com>
2020-04-17 15:39:17 +0530

    Improved Performance in v_02

--- v02/rnn-v01beta.py
+++ v02/rnn-v01beta.py
@@ -5,7 +5,6 @@ Created on Sun Apr 12 21:27:25 2020
 @author: Dell
 """
 import numpy as np
-import os
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
@@ -27,13 +26,13 @@ sc = MinMaxScaler(feature_range=(0, 1))
 trained_cases_scaled = sc.fit_transform(C)
 trained_fatalities_scaled = sc.fit_transform(F)
 
-# Creating a data structure with 20 timestamps and 1 output
+# Creating a data structure with 5 timestamps and 1 output
 CX_train = []
 CY_train = []
 #FX_train = []
 #FY_train = []
-for i in range(20, 67):
-    CX_train.append(trained_cases_scaled[i-20:i, 0])
+for i in range(5, 67):
+    CX_train.append(trained_cases_scaled[i-5:i, 0])
     CY_train.append(trained_cases_scaled[i, 0])
 #    FX_train.append(trained_fatalities_scaled[i-20:i, 0])
 #    FY_train.append(trained_fatalities_scaled[i-20:i, 0])
@@ -43,20 +42,26 @@ CX_train, CY_train = np.array(CX_train), np.array(CY_train)
 
 # Reshaping
 CX_train = np.reshape(CX_train, (CX_train.shape[0], CX_train.shape[1], 1))
-CX_val, CY_val = CX_train[40:47, :, :], CY_train[40:]
-CX_train, CY_train = CX_train[0:40, :, :], CY_train[0:40]
+CX_val, CY_val = CX_train[56:62, :, :], CY_train[56:]
+CX_train, CY_train = CX_train[0:56, :, :], CY_train[0:56]
 #FX_train = np.reshape(FX_train, (FX_train.shape[0], FX_train.shape[1], 1))# remember to add one more layer
 
 # Initialize the model
 model = Sequential()
 
 # Adding the first LSTM layer and some Dropout Regularization
-model.add(LSTM(units=15, return_sequences=True, input_shape=(CX_train.shape[1], 1)))
-#model.add(Dropout(0.2))
+model.add(LSTM(units=5, return_sequences=True, input_shape=(CX_train.shape[1], 1)))
+model.add(Dropout(0.2))
 
 # Adding the second LSTM layer and some Dropout Regularization
-model.add(LSTM(units=15))
-#model.add(Dropout(0.2))
+model.add(LSTM(units=3))
+model.add(Dropout(0.2))
+
+# Adding the first Dense layer
+model.add(Dense(units=3))
+
+# Adding the second Dense layer
+model.add(Dense(units=1))
 
 # Adding the output layer
 model.add(Dense(units=1, activation='relu'))
@@ -67,8 +72,8 @@ model.add(Dense(units=1, activation='relu'))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Fitting RNN to the training set
-model.fit(CX_train, CY_train, epochs=50, batch_size=5, validation_data=(CX_val, CY_val))
+model.fit(CX_train, CY_train, epochs=100, validation_data=(CX_val, CY_val))
 
-p1 = 'Afghanistan/nan/trained_weights.h5'
+p1 = 'Afghanistan/nan/trained_weights_100.h5'
 model.save_weights(p1)
 print ('Model saved at: '+str(p1))
--- v02/rnn-v01testbeta.py
+++ v02/rnn-v01testbeta.py
@@ -22,19 +22,25 @@ dataset = pd.read_csv('Afghanistan/nan/test.csv')
 X = dataset.iloc[:, 1:5].values
 train = pd.read_csv('Afghanistan/nan/dataset.csv')
 
-ds = train.iloc[47:, 5:6].values
+ds = train.iloc[62:, 5:6].values
 
     
 # Initialize the model
 model = Sequential()
 
 # Adding the first LSTM layer and some Dropout Regularization
-model.add(LSTM(units=15, return_sequences=True, input_shape=(20, 1)))
+model.add(LSTM(units=5, return_sequences=True, input_shape=(5, 1)))
 model.add(Dropout(0.2))
 
 # Adding the second LSTM layer and some Dropout Regularization
-model.add(LSTM(units=15))
+model.add(LSTM(units=3))
 model.add(Dropout(0.2))
+
+# Adding the first Dense layer
+model.add(Dense(units=3))
+
+# Adding the second Dense layer
+model.add(Dense(units=1))
 
 # Adding the output layer
 model.add(Dense(units=1, activation='relu'))
@@ -44,7 +50,7 @@ model.add(Dense(units=1, activation='relu'))
 # Compiling the RNN
 model.compile(optimizer='adam', loss='mean_squared_error')
 
-model_path = 'Afghanistan/nan/trained_weights.h5'
+model_path = 'Afghanistan/nan/trained_weights_100.h5'
 model.load_weights(model_path, by_name=True)
 
 
@@ -54,10 +60,10 @@ scaled = open("Afghanistan/nan/scaled.pickle", "rb")
 sc = pickle.load(scaled)
 test = sc.transform(test)
 
-for i in range(20, 63):
-    X_test = test[i-20:i, 0]
+for i in range(5, 63):
+    X_test = test[i-5:i, 0]
     
-    CX_test = np.reshape(X_test, (1, 20, 1))
+    CX_test = np.reshape(X_test, (1, 5, 1))
     pred = model.predict(CX_test)
     test = np.append(test, pred[0][0])
     test = test.reshape(-1, 1)
